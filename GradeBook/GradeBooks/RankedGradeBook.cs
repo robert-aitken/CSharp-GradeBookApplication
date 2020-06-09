e04471701592fcc1ff88795b00effcfc79e358ca
Robert A <robertleighaitken@ymail.com>
2020-06-09 21:14:28 +0100

    Commit for Top 20 Percent Get An A

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -14,20 +15,42 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            var students = Students;
-
-            if(students.Count > 5)
+            if(Students.Count > 5)
             {
-                if (averageGrade >= 20)
+                // Get the average grades for the students in a list order decending
+                var averageGrades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+                // Top 20 % - Round averageGrades
+                var threshold = (int)Math.Ceiling(Students.Count * 0.20);
+
+                /*
+                If the threshold of all the average* grades*(-1 to return index) is less than the average *grade* we passed in then we will return grade A
+                E.g. 100 average grades
+                      0.20 threshold
+                      100 * 0.20 = 20
+                      threshold = 20
+                      Index to find = 19
+                if the average grade at index 19 is less than or equal to averageGrade then we return A
+                */
+
+                if (averageGrades[threshold -1] <= averageGrade)
+                {
                     return 'A';
-                else if (averageGrade >= 80)
+                }
+                else if(averageGrades[(threshold*2) -1] <= averageGrade)
+                {
                     return 'B';
-                else if (averageGrade >= 70)
+                }
+                else if (averageGrades[(threshold * 3) - 1] <= averageGrade)
+                {
                     return 'C';
-                else if (averageGrade >= 60)
+                }
+                else if (averageGrades[(threshold * 4) - 1] <= averageGrade)
+                {
                     return 'D';
+                }
             }
-            else if (students.Count <5)
+            else if (Students.Count <5)
             {
                 throw new InvalidOperationException("Ranked - grading requires a minimum of 5 students to work");
             }
