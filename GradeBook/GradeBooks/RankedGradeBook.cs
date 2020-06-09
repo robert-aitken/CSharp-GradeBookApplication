de704ba77f5eb426edb9f7c134d558d398c55413
Robert A <robertleighaitken@ymail.com>
2020-06-09 17:04:44 +0100

    Commit for Create GetLetterGrade Override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,21 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            var students = Students;
+
+            if(students.Count > 5)
+            {
+                return base.GetLetterGrade(averageGrade);
+            }
+            else if (students.Count <5)
+            {
+                throw new InvalidOperationException("Ranked - grading requires a minimum of 5 students to work");
+            }
+
+            return 'F';
+        }
     }
 }
