5da5dc819e5c6b59c6b7b5a1e11d91ed15b05b5f
Robert A <robertleighaitken@ymail.com>
2020-06-09 17:05:56 +0100

    Commit for Create GetLetterGrade Override - Override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -18,7 +18,16 @@ namespace GradeBook.GradeBooks
 
             if(students.Count > 5)
             {
-                return base.GetLetterGrade(averageGrade);
+                if (averageGrade >= 90)
+                    return 'A';
+                else if (averageGrade >= 80)
+                    return 'B';
+                else if (averageGrade >= 70)
+                    return 'C';
+                else if (averageGrade >= 60)
+                    return 'D';
+                else
+                    return 'F';
             }
             else if (students.Count <5)
             {
@@ -26,6 +35,8 @@ namespace GradeBook.GradeBooks
             }
 
             return 'F';
+
+
         }
     }
 }
